e0f737cc0fdab3c7f6fc38b722666861a37f0082
Pedro Teles <pedroateles@protonmail.com>
2020-11-02 12:16:43 -0300

    converting deezer link to spotify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import os
+import re
 
 
 def get_track_id(spotify_url):
@@ -55,6 +56,47 @@ def get_deezer_music_link(info):
     print('Deezer link: ' + track['link'])
 
 
+def get_deezer_track_id(deezer_url):
+    deezer_response = requests.get(deezer_url)
+    track_link = re.search("(?P<url>https?://[^\s]+/track/[0-9]+)", deezer_response.text).group("url")
+    link_info = track_link.split('/')
+
+    return link_info[len(link_info) - 1]
+
+
+def get_deezer_info(track_id):
+    deezer_url = 'https://api.deezer.com/track/' + track_id
+    deezer_track_info_response = requests.get(deezer_url)
+    json_data = deezer_track_info_response.json()
+
+    album_data = json_data['album']
+    artist_data = json_data['artist']
+
+    track_name = json_data['title']
+    album_name = album_data['title']
+    artist_name = artist_data['name']
+
+    print('Track: ' + track_name)
+    print('Artist: ' + artist_name)
+    print('Album: ' + album_name)
+
+    return {'track': track_name, 'artist': artist_name, 'album': album_name}
+
+
+def get_spotify_music_link(info, token):
+    query_value = info['track'] + ' artist: ' + info['artist'] + ' album: ' + info['album']
+    params = {'q': query_value, 'type': 'track'}
+    headers = {'Authorization': 'Bearer ' + token}
+    spotify_search_response = requests.get('https://api.spotify.com/v1/search', params=params, headers=headers)
+    json_data = spotify_search_response.json()
+
+    tracks = json_data['tracks']
+    items_data = tracks['items'][0]
+    external_urls = items_data['external_urls']
+
+    print('Spotify link: ' + external_urls['spotify'])
+
+
 music_url = input('Enter the music URL: ')
 
 if music_url.__contains__('spotify'):
@@ -65,6 +107,12 @@ if music_url.__contains__('spotify'):
 
     get_deezer_music_link(track_info)
 elif music_url.__contains__('deezer'):
-    print('not implemented')
+    print('Converting Deezer link to Spotify link...')
+    spotify_token = get_spotify_token()
+    deezer_track_id = get_deezer_track_id(music_url)
+    deezer_track_info = get_deezer_info(deezer_track_id)
+
+    get_spotify_music_link(deezer_track_info, spotify_token)
+
 else:
     print('Unknown music platform')
