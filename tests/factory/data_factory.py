5d88bb4c68de94850cfe4445832472587de35ada
Pedro Teles <pedroateles@protonmail.com>
2021-08-01 16:48:48 -0300

    finish tests in spotify.py

--- tests/factory/data_factory.py
+++ tests/factory/data_factory.py
@@ -16,3 +16,19 @@ def mock_failure_spotify_token_response():
     response._content = None
 
     return response
+
+
+def mock_success_spotify_search_music_response():
+    response = Response()
+    response.status_code = 200
+    response._content = json_helper.get_json_mock('spotify_search_response_success.json')
+
+    return response
+
+
+def mock_success_spotify_track_detail_response():
+    response = Response()
+    response.status_code = 200
+    response._content = json_helper.get_json_mock('spotify_track_detail_response_success.json')
+
+    return response
--- tests/test_spotify.py
+++ tests/test_spotify.py
@@ -1,6 +1,5 @@
 import os
 from unittest import TestCase
-from unittest.mock import patch
 from mockito import kwargs
 
 import requests
@@ -22,7 +21,7 @@ class SpotifyTest(TestCase):
         spotify.generate_spotify_authorization(client_id, client_secret)
 
         # Then
-        self.assertEquals(os.environ['SPOTIFY_AUTHORIZATION'], 'dGVzdDp0ZXN0')
+        self.assertEqual(os.environ['SPOTIFY_AUTHORIZATION'], 'dGVzdDp0ZXN0')
 
     def test_when_pass_spotify_url_should_return_track_id(self):
         # Given
@@ -32,7 +31,7 @@ class SpotifyTest(TestCase):
         track_id = spotify.get_track_id(spotify_url)
 
         # Then
-        self.assertEquals(track_id, '12345')
+        self.assertEqual(track_id, '12345')
 
     def test_when_configure_authorization_correctly_should_return_true(self):
         # Given
@@ -68,13 +67,33 @@ class SpotifyTest(TestCase):
 
     def test_when_pass_valid_token_should_return_true(self):
         # Given
-        invalid_token = 'token'
+        valid_token = 'token'
 
         # When
-        is_valid = spotify.is_token_valid(invalid_token)
+        is_valid = spotify.is_token_valid(valid_token)
 
         # Then
         self.assertTrue(is_valid)
+
+    def test_when_spotify_authorization_is_configured_should_return_true(self):
+        # Given
+        os.environ['SPOTIFY_AUTHORIZATION'] = 'token'
+
+        # When
+        is_valid = spotify.has_spotify_authorization()
+
+        # Then
+        self.assertTrue(is_valid)
+
+    def test_when_spotify_authorization_is_not_configured_should_return_false(self):
+        # Given
+        os.environ.clear()
+
+        # When
+        is_valid = spotify.has_spotify_authorization()
+
+        # Then
+        self.assertFalse(is_valid)
 
     def test_when_request_successfully_should_return_access_token(self):
         # Given
@@ -87,7 +106,7 @@ class SpotifyTest(TestCase):
         token = spotify.get_spotify_token()
 
         # Then
-        self.assertEquals(token, 'BQAwALn7Six_RhtNu4LWHnffT7Zsw55uKRxsxtrdjbHRvKFINHhBeI4cGQKBBwwmh88l6T9fQHDbbYiw_zI')
+        self.assertEqual(token, 'BQAwALn7Six_RhtNu4LWHnffT7Zsw55uKRxsxtrdjbHRvKFINHhBeI4cGQKBBwwmh88l6T9fQHDbbYiw_zI')
 
     def test_when_request_fails_should_return_empty_token(self):
         # Given
@@ -100,4 +119,34 @@ class SpotifyTest(TestCase):
         token = spotify.get_spotify_token()
 
         # Then
-        self.assertEquals(token, '')
+        self.assertEqual(token, '')
+
+    def test_when_request_search_music_should_return_url(self):
+        # Given
+        token = 'test'
+        track_info = {'track': 'Python Unit Test', 'artist': 'Python', 'album': 'Unit Testing'}
+        when(requests).get('https://api.spotify.com/v1/search', **kwargs).thenReturn(
+            data_factory.mock_success_spotify_search_music_response()
+        )
+
+        # When
+        url = spotify.get_spotify_music_link(track_info, token)
+
+        # Then
+        self.assertEqual(url, 'https://open.spotify.com/track/UnitTest')
+
+    def test_when_request_track_info_should_return_dictionary(self):
+        # Given
+        token = 'token'
+        track = 'track'
+        when(requests).get('https://api.spotify.com/v1/tracks/' + track, **kwargs).thenReturn(
+            data_factory.mock_success_spotify_track_detail_response()
+        )
+
+        # When
+        details = spotify.get_spotify_track_info(token, track)
+
+        # Then
+        self.assertEqual(details['artist'], 'Python')
+        self.assertEqual(details['album'], 'Unit Testing')
+        self.assertEqual(details['track'], 'Python Unit Test')
